78462d661d17a500c9d314a325e09c11440090b3
romaric <coulromaric@gmail.comexit>
2024-01-25 20:55:14 +0000

    responsive okay

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 
-import { Button} from '@chakra-ui/react'
-
+import { Button, Show} from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
 function App() {
   
 
   return (
     <>
-      <Button colorScheme='blue'>Button</Button>
+    <Grid templateAreas={{base:`"nav main"`,lg:`"nav nav" "aside main"`}}>
+    <GridItem area={"nav"} backgroundColor="coral">
+    nav
+    </GridItem>
+    <Show above='lg'>
+    <GridItem area={"aside"} backgroundColor ="gold">
+    aside
+    </GridItem>
+    </Show>
+    <GridItem area={"main"} backgroundColor="orange">
+      main
+    </GridItem>
+    
+    </Grid>
+      
     </>
   )
 }
