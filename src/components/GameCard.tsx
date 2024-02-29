4a106e80f419e5ca7c520d0fe7bc0fb8f907ee3c
romaric <coulromaric@gmail.comexit>
2024-02-29 19:37:45 +0000

    clean up the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -18,11 +18,11 @@ const GameCard = ({game}:Props) => {
     <Card >
         <Image src={getCroppedImageUrl(game.background_image)}/>
         <CardBody>
-            <Heading size={"lg"}>{game.name}</Heading>
-            <HStack justifyContent={"space-between"}>
+            <HStack justifyContent={"space-between"} marginBottom={3}>
             <PlatformIconList platforms={game.parent_platforms.map( ({platform}) => platform)}/>
             <CriticScore score={game.metacritic}/>
             </HStack>
+            <Heading fontSize={"2xl"}>{game.name}</Heading>
         </CardBody>
     </Card>
   )
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -18,7 +18,7 @@ const skeleton = [1,2,3,4,5,6,7,8,9,10]
   return (
     <>
     {error&&<Text>{error}</Text>}
-    <SimpleGrid columns={{sm:1,md:2,lg:3,xl:5}} spacing={3} padding={"10px"}>
+    <SimpleGrid columns={{sm:1,md:2,lg:3,xl:4}} spacing={3} padding={"10px"}>
     {isLoading&&skeleton.map(sk => <GameCardContener key={sk}><GameCardSkeleton /></GameCardContener>)}
       {data.map((game) => (
        <GameCardContener key={game.id}>
